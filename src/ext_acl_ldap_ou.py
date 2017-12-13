b9d892eb2c9871ec410c95dcba801d3d99643269
Vadim Aleksandrov <valeksandrov@me.com>
2017-12-14 00:48:27 +0300

    Add get_ldap_connection method. Change --server option. Can be set multiple instance. Use first active strategy Change conn variable existing test

--- src/ext_acl_ldap_ou.py
+++ src/ext_acl_ldap_ou.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ldap3 import Server, Connection, AUTO_BIND_NO_TLS, SUBTREE
+from ldap3 import Server, ServerPool, Connection, FIRST, AUTO_BIND_NO_TLS, SUBTREE
 import argparse
 from os import path
 import sys
 
 
-def get_ldap_info(server='', port='', tls=False, timeout=0, timelimit=0, binddn='', bindpasswd='', ou='', basedn='', filter='', username=''):
+def get_ldap_connection(server=[], port='', ssl=False, timeout=0, binddn='', bindpasswd=''):
     try:
-        with Connection(Server(server, port, use_ssl=tls),
-                        auto_bind=AUTO_BIND_NO_TLS,
-                        read_only=True,
-                        receive_timeout=timeout,
-                        check_names=True,
-                        user=binddn, password=bindpasswd) as c:
+        server_pool = ServerPool([Server(item, port, use_ssl=ssl) for item in server],
+                                 FIRST,
+                                 active=3,
+                                 exhaust=60)
+        conn = Connection(server_pool,
+                          auto_bind=AUTO_BIND_NO_TLS,
+                          read_only=True,
+                          receive_timeout=timeout,
+                          check_names=True,
+                          user=binddn, password=bindpasswd)
+    except Exception as exc:
+        print('ext_acl_ldap_group LDAP bind error {}({})'.format(type(exc).__name__, exc))
+    else:
+        return conn
 
-            c.search(search_base=basedn.replace('%ou', ou),
-                     search_filter='(&({}))'.format(filter.replace('%v', username)),
-                     search_scope=SUBTREE,
-                     attributes=['sAMAccountName'],
-                     time_limit=timelimit,
-                     get_operational_attributes=True)
+
+def get_ldap_info(connection='', timelimit=0, binddn='', bindpasswd='', ou='', basedn='', filter='', username=''):
+    if not connection:
+        print('BH message="LDAP connection could not be established"')
+        return False
+
+    try:
+        connection.search(search_base=basedn.replace('%ou', ou),
+                          search_filter='(&({}))'.format(filter.replace('%u', username)),
+                          search_scope=SUBTREE,
+                          attributes=['sAMAccountName'],
+                          time_limit=timelimit,
+                          get_operational_attributes=True)
     except Exception as exc:
         print('BH message={}({})'.format(type(exc).__name__, exc))
     else:
-        if len(c.response) > 0:
+        if len(connection.response) > 0:
             print('OK tag="{}"'.format(ou))
         else:
             print('ERR')
 
 
-def main():
+def create_cli():
     parser = argparse.ArgumentParser(description='Squid external acl ldap ou helper')
     parser.add_argument('-d', '--binddn', type=str, required=True,
                         help='DN to bind as to perform searches')
@@ -39,29 +54,35 @@ def main():
                         help='password for binddn')
     parser.add_argument('-W', '--secretfile',
                         help='read password for binddn from file secretfile')
-    parser.add_argument('-s', '--server', type=str, default='localhost',
-                        help='LDAP server (defaults to %(default)s)')
+    parser.add_argument('-s', '--server', action='append', required=True,
+                        help='LDAP server. Can be set multiple instance. Use first active strategy')
     parser.add_argument('-p', '--port', type=int, default=389,
                         help='LDAP server port (defaults to %(default)i)')
-    parser.add_argument('-z', '--tls', action='store_true',
-                        help='TLS encrypt the LDAP connection')
+    parser.add_argument('-z', '--ssl', action='store_true',
+                        help='SSL encrypt the LDAP connection')
     parser.add_argument('-c', '--timeout', type=int, default=10,
                         help='connect timeout (defaults to %(default)i)')
     parser.add_argument('-t', '--timelimit', type=int, default=10,
                         help='search time limit (defaults to %(default)i)')
     parser.add_argument('-b', '--basedn', type=str, required=True,
-                        help='base dn under where to search for users')
+                        help='base dn under where to search for users. %%ou = OU')
     parser.add_argument('-f', '--filter', type=str, required=True,
-                        help='base dn under where to search for users')
+                        help='user search filter pattern. %%u = login')
     parser.add_argument('-k', '--strip-realm', action='store_true',
                         help='strip Kerberos realm from usernames')
     parser.add_argument('-n', '--strip-domain', action='store_true',
                         help='strip NT domain from usernames')
+    return parser
 
+
+def main():
+    parser = create_cli()
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
     args = parser.parse_args()
+    conn = None
+
     if hasattr(args, 'bindpasswd') and args.bindpasswd:
         bindpasswd = args.bindpasswd
     elif hasattr(args, 'secretfile') and args.secretfile:
@@ -79,10 +100,21 @@ def main():
         print('ext_acl_ldap_ou Password for binddn is not set')
         bindpasswd = ''
 
+    if bindpasswd:
+        conn = get_ldap_connection(server=args.server,
+                                   port=args.port,
+                                   ssl=args.ssl,
+                                   timeout=int(args.timeout),
+                                   binddn=args.binddn,
+                                   bindpasswd=bindpasswd)
+
     while 1:
         try:
             input = sys.stdin.readline().split()
         except KeyboardInterrupt:
+            if isinstance(conn, Connection):
+                if conn.bound:
+                    conn.unbind()
             sys.exit()
         try:
             username = input[0]
@@ -93,17 +125,25 @@ def main():
             elif args.strip_domain and '\\' in username:
                 username = username.split('\\')[1]
 
-            get_ldap_info(server=args.server,
-                          port=args.port,
-                          tls=args.tls,
-                          timeout=int(args.timeout),
-                          timelimit=int(args.timelimit),
-                          binddn=args.binddn,
-                          bindpasswd=bindpasswd,
-                          ou=ou,
-                          basedn=args.basedn,
-                          filter=args.filter,
-                          username=username)
+            if isinstance(conn, Connection):
+                if conn.closed:
+                    conn = get_ldap_connection(server=args.server,
+                                               port=args.port,
+                                               tls=args.tls,
+                                               timeout=int(args.timeout),
+                                               binddn=args.binddn,
+                                               bindpasswd=bindpasswd)
+                if conn.bound:
+                    get_ldap_info(connection=conn,
+                                  timelimit=int(args.timelimit),
+                                  binddn=args.binddn,
+                                  bindpasswd=bindpasswd,
+                                  ou=ou,
+                                  basedn=args.basedn,
+                                  filter=args.filter,
+                                  username=username)
+            else:
+                print('BH message="LDAP connection could not be established"')
             sys.stdout.flush()
         except:
             continue
