7e004489ac23587ce1459a6668b26261f5fbbbd2
Vadim Aleksandrov <valeksandrov@me.com>
2018-01-15 22:07:54 +0300

    Change connection pool keepalive interval to 60 second

--- src/ext_acl_ldap_ou.py
+++ src/ext_acl_ldap_ou.py
@@ -14,7 +14,7 @@ def get_ldap_connection(server=[], port='', ssl=False, timeout=0, binddn='', bin
                                  active=3,
                                  exhaust=60)
         conn = Connection(server_pool,
-                          pool_keepalive=600,
+                          pool_keepalive=60,
                           auto_bind=AUTO_BIND_NO_TLS,
                           read_only=True,
                           receive_timeout=timeout,
